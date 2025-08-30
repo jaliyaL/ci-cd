01b76521d8c29044d5c3a4b1d654a4c438233caa
Jaliya <jaliyalamahewa@gmail.com>
2025-08-30 23:56:37 +0530

    test change

--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestHelloWorld(t *testing.T) {
 
 	expected := "hello CI/CD"
-	got := "hello CI/CD"
+	got := "hello CI/CD1"
 
 	if got != expected {
 		t.Errorf("got %s, want %s", got, expected)
