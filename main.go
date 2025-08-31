277771a2b89e2f2956392fba2896b1b549739d2b
Jaliya <jaliyalamahewa@gmail.com>
2025-08-31 10:08:05 +0530

    go sum

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
 
 func main() {
+
+	id := uuid.New()
+	fmt.Println("Hello World! Your UUID is:", id.String())
+
 	fmt.Println("hello CI/CD")
 }
